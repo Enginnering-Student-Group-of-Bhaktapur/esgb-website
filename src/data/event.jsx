552e2f31e80df8a710bff381c246a13269050534
Gaurav Jyakhwa <gjyakhwa1@gmail.com>
2024-02-14 10:37:09 +0545

    Changa chet and hike ccontent updated

--- src/data/event.jsx
+++ src/data/event.jsx
@@ -1,37 +1,37 @@
-import orientation1 from "../../src/assets/img/orientation/1.jpg"
-import orientation2 from "../../src/assets/img/orientation/2.jpg"
-import orientation3 from "../../src/assets/img/orientation/3.jpg"
+import orientation1 from "../../src/assets/img/orientation/1.jpg";
+import orientation2 from "../../src/assets/img/orientation/2.jpg";
+import orientation3 from "../../src/assets/img/orientation/3.jpg";
 
-import deusi1 from "../../src/assets/img/deusi_bhailo/1.jpg"
-import deusi2 from "../../src/assets/img/deusi_bhailo/2.jpg"
-import deusi3 from "../../src/assets/img/deusi_bhailo/3.jpg"
-import deusi4 from "../../src/assets/img/deusi_bhailo/4.jpg"
-import deusi5 from "../../src/assets/img/deusi_bhailo/5.jpg"
-import deusi6 from "../../src/assets/img/deusi_bhailo/6.jpg"
+import deusi1 from "../../src/assets/img/deusi_bhailo/1.jpg";
+import deusi2 from "../../src/assets/img/deusi_bhailo/2.jpg";
+import deusi3 from "../../src/assets/img/deusi_bhailo/3.jpg";
+import deusi4 from "../../src/assets/img/deusi_bhailo/4.jpg";
+import deusi5 from "../../src/assets/img/deusi_bhailo/5.jpg";
+import deusi6 from "../../src/assets/img/deusi_bhailo/6.jpg";
 
-import kora1 from "../../src/assets/img/kora/1.jpg"
-import kora2 from "../../src/assets/img/kora/2.jpg"
+import kora1 from "../../src/assets/img/kora/1.jpg";
+import kora2 from "../../src/assets/img/kora/2.jpg";
 
-import futsal1 from "../../src/assets/img/futsal/3.jpg"
-import futsal2 from "../../src/assets/img/futsal/9.jpg"
-import futsal3 from "../../src/assets/img/futsal/14.jpg"
+import futsal1 from "../../src/assets/img/futsal/3.jpg";
+import futsal2 from "../../src/assets/img/futsal/9.jpg";
+import futsal3 from "../../src/assets/img/futsal/14.jpg";
 
-import welcome1 from "../../src/assets/img/welcome_farewell/1.jpg"
-import welcome2 from "../../src/assets/img/welcome_farewell/2.jpg"
-import welcome3 from "../../src/assets/img/welcome_farewell/3.jpg"
-import welcome4 from "../../src/assets/img/welcome_farewell/4.jpg"
-import welcome5 from "../../src/assets/img/welcome_farewell/5.jpg"
+import welcome1 from "../../src/assets/img/welcome_farewell/1.jpg";
+import welcome2 from "../../src/assets/img/welcome_farewell/2.jpg";
+import welcome3 from "../../src/assets/img/welcome_farewell/3.jpg";
+import welcome4 from "../../src/assets/img/welcome_farewell/4.jpg";
+import welcome5 from "../../src/assets/img/welcome_farewell/5.jpg";
 
-import hike1 from "../../src/assets/img/hike/1.jpg"
-import hike2 from "../../src/assets/img/hike/2.jpg"
-import hike3 from "../../src/assets/img/hike/3.jpg"
-import hike4 from "../../src/assets/img/hike/4.jpg"
-import hike5 from "../../src/assets/img/hike/5.jpeg"
+import hike1 from "../../src/assets/img/hike/1.jpg";
+import hike2 from "../../src/assets/img/hike/2.jpg";
+import hike3 from "../../src/assets/img/hike/3.jpg";
+import hike4 from "../../src/assets/img/hike/4.jpg";
+import hike5 from "../../src/assets/img/hike/5.jpeg";
 
-import changa1 from "../../src/assets/img/changa_chet/1.jpg"
-import changa2 from "../../src/assets/img/changa_chet/2.jpg"
-import changa3 from "../../src/assets/img/changa_chet/3.jpg"
-import changa4 from "../../src/assets/img/changa_chet/4.jpg"
+import changa1 from "../../src/assets/img/changa_chet/1.jpg";
+import changa2 from "../../src/assets/img/changa_chet/2.jpg";
+import changa3 from "../../src/assets/img/changa_chet/3.jpg";
+import changa4 from "../../src/assets/img/changa_chet/4.jpg";
 
 const events_list = [
   {
@@ -76,7 +76,7 @@ const events_list = [
     is_display: true,
     is_featured: true,
     main_image: kora2,
-    image_1:kora1,
+    image_1: kora1,
     image_2: kora2,
   },
   {
@@ -136,10 +136,11 @@ const events_list = [
     date_of_event: "2023-12-30",
     venue: "Sundarijal",
     description:
-      "The Welcome and Farewell Program of ESGB is a cherished tradition within our organization, serving as a symbolic transition between outgoing and incoming leaders. It is a moment of reflection, appreciation, and anticipation as we celebrate the accomplishments of the past while looking forward to the future with renewed enthusiasm and vigor.\
-      The welcome program marks the beginning of their journey, infused with the spirit of unity and a commitment to uphold the values and ideals of our organization.\
-      Amidst the excitement of new beginnings, we pause to pay tribute to the departing members of the committee. Their dedication, leadership, and tireless efforts have been the cornerstone of our organization's success.\
-      In essence, the Welcome and Farewell Program of ESGB is more  a celebration of leadership, friendship, and the unwavering commitment to make a difference in the world around us.",
+      "Engineering Students' Group of Bhaktapur (ESGB) organizes a hiking program every year following the vibrant Deusi Bhailo program as a refreshing break in nature. \
+      Walking the trails together, reaching the destination, and enjoying nature along with the newari khaja in the hike also promotes bonding and interaction among the students and the seniors. \
+      Some of the past hiking destinations are Champadevi, Tarkeshwor to Suryachaur to Budhanilkantha, and Sundarijal to Dhap Dam.\
+      Main highlights of this event are huge participation exceeding 50, newari khaja set reflecting our unique culture including Yomari, and networking and interaction among the participants.\
+      As the destination is much far, ESGB hires a bus and the journey in the bus is filled with songs, and discussions making the journey more mesmerizing. With the conversations among the groups, sharing stories and memories, the hiking is less exhausting and more fun. This event is one of the most exciting, memorable, and refreshing experiences that also strengthens the bond between ESGBians.",
     is_display: true,
     is_featured: false,
     main_image: hike3,
@@ -155,10 +156,9 @@ const events_list = [
     date_of_event: "2023-10-14",
     venue: "Bhakatapur",
     description:
-      "The Welcome and Farewell Program of ESGB is a cherished tradition within our organization, serving as a symbolic transition between outgoing and incoming leaders. It is a moment of reflection, appreciation, and anticipation as we celebrate the accomplishments of the past while looking forward to the future with renewed enthusiasm and vigor.\
-      The welcome program marks the beginning of their journey, infused with the spirit of unity and a commitment to uphold the values and ideals of our organization.\
-      Amidst the excitement of new beginnings, we pause to pay tribute to the departing members of the committee. Their dedication, leadership, and tireless efforts have been the cornerstone of our organization's success.\
-      In essence, the Welcome and Farewell Program of ESGB is more  a celebration of leadership, friendship, and the unwavering commitment to make a difference in the world around us.",
+      "Changa Chet Program is organized by ESGB (Engineering Students' Group of Bhaktapur) during the festive occasion of Dashain, two or three days prior to Bijaya Dashami. This event holds significant cultural importance as Dashain is the biggest festival of Nepal and it offers students a unique opportunity to participate actively in traditional festivities. \
+      The Changa Chet Program typically commences with the exhilarating activity of flying kites, a cherished tradition during Dashain celebrations followed by tika and blessings from esteemed seniors which reinforces the bond between seniors and juniors. The colorful kites soaring the clear skies of Dashain brings immense joy filling hearts with happiness and excitement among all the participants.\
+      Other major highlights of the events are live musical performance, Newari Khaja, and other fun activities creating a vibrant atmosphere and reminding of the values of respect, gratitude, and cherishing bonds and kinship.",
     is_display: true,
     is_featured: false,
     main_image: changa4,
