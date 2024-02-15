59bcdf7e0f6298dee39f232e26c28008bf775300
Gaurav Jyakhwa <gjyakhwa1@gmail.com>
2024-02-15 17:17:58 +0545

    committe members images added

--- src/data/member_data.jsx
+++ src/data/member_data.jsx
@@ -1,15 +1,20 @@
-import banner_1 from "../assets/img/HomeCarousel/1.jpg";
+import anish_gasi from "../assets/img/committee/anish_gasi.png"
+import anuj_shrestha from "../assets/img/committee/anuj_shrestha.png"
+import punam_pradhan from "../assets/img/committee/punam_pradhan.png"
+import rohan_bade from "../assets/img/committee/rohan_bade.png"
+import sumi_shakya from "../assets/img/committee/sumi_shakya.png"
+import banner_1 from "../assets/img/HomeCarousel/1.jpg"
 
 const members = [
+  { name: "Anish Gasi", position: "President", image: anish_gasi },
   { name: "Ishani Malla", position: "Vice President", image: banner_1 },
-  { name: "Anish Gasi", position: "President", image: banner_1 },
   { name: "Kishor Gurung", position: "Vice President", image: banner_1 },
   { name: "Sushil Duwal", position: "Joint Secretary", image: banner_1 },
-  { name: "Rohan Bade", position: "Secretary", image: banner_1 },
-  { name: "Sumi Shakya", position: "Joint Secretary", image: banner_1 },
+  { name: "Rohan Bade", position: "Secretary", image: rohan_bade },
+  { name: "Sumi Shakya", position: "Joint Secretary", image: sumi_shakya },
   { name: "Rohan Suwal", position: "Treasurer", image: banner_1 },
-  { name: "Punam Pradhan", position: "Social Media Manager", image: banner_1 },
-  { name: "Anuj Shrestha", position: "Creative Head", image: banner_1 },
+  { name: "Punam Pradhan", position: "Social Media Manager", image: punam_pradhan },
+  { name: "Anuj Shrestha", position: "Creative Head", image: anuj_shrestha },
 ];
 
 export default members;
